67a9f63609158f164bf54c850d53f3d0ae4f7d60
ljaureguil <33967760+ljaureguil@users.noreply.github.com>
2021-09-30 13:39:56 -0700

    Update qr.js

--- qr.js
+++ qr.js
@@ -1,6 +1,6 @@
 
 
-function(t, e) {
+!function(t, e) {
     "object" == typeof exports && "undefined" != typeof module ? module.exports = e() : "function" == typeof define && define.amd ? define(e) : t.QRious = e()
 }(this, function() {
     "use strict";
