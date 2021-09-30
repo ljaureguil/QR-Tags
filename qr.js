f067b8eea32d52b2a10f633ff514a40b9d0ac208
ljaureguil <33967760+ljaureguil@users.noreply.github.com>
2021-09-30 07:20:44 -0700

    Update qr.js

--- qr.js
+++ qr.js
@@ -1,8 +1,6 @@
 
-/*! QRious v4.0.2 | (C) 2017 Alasdair Mercer | GPL v3 License
-Based on jsqrencode | (C) 2010 tz@execpc.com | GPL v3 License
-*/
-! function(t, e) {
+
+function(t, e) {
     "object" == typeof exports && "undefined" != typeof module ? module.exports = e() : "function" == typeof define && define.amd ? define(e) : t.QRious = e()
 }(this, function() {
     "use strict";
